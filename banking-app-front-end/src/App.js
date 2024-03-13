7a0adb45a1241afd274db1e80d90b48fbcd3799e
Brockosh <brockjoshea@gmail.com>
2024-03-13 16:23:27 +1100

    Hooks error resolved.

---
+++ banking-app-front-end/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import AuthOptions from './AuthOptions';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <h1> Banking Application </h1>
+        <AuthOptions/>
+      </div> 
+    </div>
+  );
+}
+
+export default App;
